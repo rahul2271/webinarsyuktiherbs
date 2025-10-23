586c466866eea6b5084bb592b1298d7d1d758d24
Rahul2271 <rraahhuullch@gmail.com>
2025-10-23 12:48:22 +0530

    d

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./public/**/*.html",
+    './app/**/*.{js,ts,jsx,tsx}',   // all files inside app folder
+    './pages/**/*.{js,ts,jsx,tsx}', // if you have pages folder
+    './components/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
