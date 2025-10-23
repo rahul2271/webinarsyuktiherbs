555d9f87e9902e848c37534a71d37bad3dfd0e58
Rahul2271 <rraahhuullch@gmail.com>
2025-10-23 12:07:52 +0530

    S

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,9 @@
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./public/**/*.html",
   ],
   theme: {
     extend: {},
